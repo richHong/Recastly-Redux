8f18f606c1cf96ea0cf7036ffcfc735fc68ef388
Richard Hong <rich.hong@gmail.com>
2016-07-13 19:03:08 -0700

    code cleanup

--- public/src/components/Nav.js
+++ public/src/components/Nav.js
@@ -4,7 +4,7 @@ import Search from './search';
 const Nav = ({ search }) => (
   <nav className="navbar">
     <div className="col-md-6 col-md-offset-3">
-      <Search search={search}/>
+      <Search search={ search }/>
     </div>
   </nav>
 );
